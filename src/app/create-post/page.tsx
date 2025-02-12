ec214347dce309145e567007ad21d7472527a76d
Isaac Maddox <isaacmaddox05@gmail.com>
2025-02-11 23:46:31 -0600

    Fix Light Theme

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function CreatePostPage() {
    return (
       <>
-         <header className="flex items-center justify-between min-h-64 bg-gradient-to-b from-zinc-900 to-zinc-800 dark:from-zinc-900 dark:to-zinc-950 text-zinc-50 dark:text-zinc-950">
+         <header className="flex items-center justify-between min-h-64 bg-gradient-to-b from-zinc-300 to-zinc-100 dark:from-zinc-900 dark:to-zinc-950 text-zinc-50 dark:text-zinc-950">
             <div className="wrapper">
                <h1>Create a Post</h1>
                <p className="mt-3 text-lg leading-normal">Ideas derived from dreams are welcome!</p>
