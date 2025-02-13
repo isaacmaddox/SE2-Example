136d788c5840f886ea1bf7279020c64e4b29a0ee
Isaac Maddox <isaacmaddox05@gmail.com>
2025-02-12 23:34:14 -0600

    DAO

---
+++ src/entities/post/dao.ts
@@ -0,0 +1,39 @@
+import { prisma } from "@/lib/prisma";
+
+export const PostDAO = {
+   async getPosts() {
+      return prisma.post.findMany({
+         orderBy: {
+            createdAt: "desc",
+         },
+         where: {
+            parentId: null,
+         },
+      });
+   },
+
+   async createPost(title: string, content: string) {
+      return prisma.post.create({
+         data: { title, content },
+      });
+   },
+
+   async getPost(id: number) {
+      return prisma.post.findUnique({
+         where: { id },
+      });
+   },
+
+   async getPostWithComments(id: number) {
+      return prisma.post.findUnique({
+         where: { id },
+         include: { comments: { orderBy: { createdAt: "desc" } } },
+      });
+   },
+
+   async createComment(postId: number, content: string) {
+      return prisma.post.create({
+         data: { content, parent: { connect: { id: postId } } },
+      });
+   },
+};
